37003de405d25ba9789c93b64971d03bbabb98cd
Malfaa <gmalfacorrea@gmail.com>
2023-05-15 19:47:35 -0300

    feat: Minor changes
    Resolução: Sem resolução.

--- app/src/main/java/com/malfa/bancodefilmes/compose/cartaz/PosterFragment.kt
+++ app/src/main/java/com/malfa/bancodefilmes/compose/cartaz/PosterFragment.kt
@@ -1,16 +1,19 @@
 package com.malfa.bancodefilmes.compose.cartaz
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
@@ -71,24 +74,26 @@ fun Teste(modifier: Modifier = Modifier){ //todo entender como que funciona o la
 
     Column(
         modifier = modifier
-            .padding(80.dp)
+//            .padding(start = 80.dp, end = 80.dp, bottom = 80.dp, top = 40.dp)
             .background(cartaz_bg)
     ) {
-        SubcomposeAsyncImage(
+        AsyncImage(
             model = "https://m.media-amazon.com/images/M/MV5BOGEwMTQyMDktMWUwZC00MzExLTg1MGMtYWJiNWNhMzIyMGU5XkEyXkFqcGdeQXVyOTYyMTY2NzQ@._V1_SX300.jpg",
-            contentDescription = null
+            contentDescription = null,
+            alignment = Alignment.Center,
+            modifier = modifier
+                .height(300.dp)
+                .width(300.dp)
         )
 
-        Row(modifier = modifier
 
-        ) {
-            Text(text = "Ice Age")
-            Spacer(modifier = modifier.width(20.dp))
-
-            SubcomposeAsyncImage(model = R.drawable.livre_vetor, contentDescription = null)
-
-        }
 
         Text(text = "Aventura")
+    }
+    Row(modifier = modifier) {
+        Text(text = "Ice Age")
+        Spacer(modifier = modifier.width(20.dp))
+        Image(painterResource(id = R.drawable.livre_vetor), contentDescription = null)
+
     }
 }
